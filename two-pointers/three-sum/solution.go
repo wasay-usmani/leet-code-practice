77dbc00a0b5af353eb907f7c62384afbfd7f654c
Wasay Usmani <wasayusmani@hotmail.com>
2025-06-18 16:46:01 +0500

    threesome

---
+++ two-pointers/three-sum/solution.go
@@ -0,0 +1,37 @@
+package threesum
+
+import "sort"
+
+func ThreeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	var res [][]int
+	n := len(nums)
+	for i := 0; i < n-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, n-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				res = append(res, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+
+			} else if sum < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+	return res
+}
---
+++ two-pointers/three-sum/solution_test.go
@@ -0,0 +1,51 @@
+package threesum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestThreeSum(t *testing.T) {
+	testCases := []struct {
+		name     string
+		nums     []int
+		expected [][]int
+	}{
+		{"Example 1", []int{-2, 0, 1, 1, 2}, [][]int{{-2, 0, 2}, {-2, 1, 1}}},
+		{"Example 2", []int{0, 1, 1}, [][]int{}},
+		{"Example 3", []int{0, 0, 0}, [][]int{{0, 0, 0}}},
+		{"No solution", []int{1, 2, 3}, [][]int{}},
+		{"All zeros", []int{0, 0, 0, 0}, [][]int{{0, 0, 0}}},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := ThreeSum(tc.nums)
+			if !tripletsEqual(result, tc.expected) {
+				t.Errorf("%s: expected %v, got %v", tc.name, tc.expected, result)
+			}
+		})
+	}
+}
+
+// Helper to check if two slices of int triplets are equal regardless of order
+func tripletsEqual(a, b [][]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	used := make([]bool, len(b))
+	for _, tripletA := range a {
+		found := false
+		for j, tripletB := range b {
+			if !used[j] && reflect.DeepEqual(tripletA, tripletB) {
+				used[j] = true
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+	return true
+}
