abe3392531d88408fe48da1a6d6d8b3cccd89117
Wasay Usmani <wasayusmani@hotmail.com>
2025-06-18 20:15:37 +0500

    Best Time to Buy and Sell Stock

--- sliding-window/best-time-to-buy-and-sell-stock/solution.go
+++ sliding-window/best-time-to-buy-and-sell-stock/solution.go
@@ -2,6 +2,24 @@ package besttimetobuyandsellstock
 
 // MaxProfit returns the maximum profit from buying and selling a stock once.
 func MaxProfit(prices []int) int {
-	// TODO: implement the solution
-	return 0
+	max, profit, left, right := 0, 0, 0, 1
+	for right < len(prices) {
+		if prices[left] <= prices[right] {
+			if profit < prices[right] {
+				profit = prices[right]
+			}
+
+			right++
+		} else if prices[left] > prices[right] {
+			profit = 0
+			left = right
+			right++
+		}
+
+		if profit > max {
+			max = profit - prices[left]
+		}
+	}
+
+	return max
 }
